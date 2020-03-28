e86e97b30c341c02ad8553de11f00d338936b794
genxhero <genxhero@users.noreply.github.com>
2020-03-28 08:58:25 -0700

    swyle project data

---
+++ src/data/project_data.js
@@ -0,0 +1,30 @@
+const project_data = [
+    { 
+        title: "Swyle",
+        image: "./images/swyle.png",
+        description: "A websocket-powered blogging platform. It uses Ruby on Rails, GraphQL, Apollo, and React.  Post your own articles, upload images, make comments, react to posts in many fun ways.",
+        github: "https://github.com/genxhero/swyle",
+        live: "https://afternoon-eyrie-69554.herokuapp.com/"
+    }, 
+    {
+        title:
+            image:
+        description:
+            github:
+        live: 
+    }, 
+    {
+        title:
+            image:
+        description:
+            github:
+        live: 
+    },
+    {
+        title:
+            image:
+        description:
+            github:
+        live: 
+    } 
+]
