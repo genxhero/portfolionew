7f1f6a0da73f01caaf298f16828b89a39d8b60bc
genxhero <genxhero@users.noreply.github.com>
2020-03-28 10:20:23 -0700

    build up projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import projects from './data/project_data';
+import Project from './Project';
 
 const Projects = () => {
 
+    return (
+        <div>
+            <div>
+                {
+                    projects.map(project => <Project project={project}/>)
+                }
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
