a9a2a24bd398600953d7e7f479014236432cddd4
genxhero <genxhero@users.noreply.github.com>
2020-03-28 09:24:52 -0700

    flesh out project basic structure

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Project = props => {
-
+    const project = {props};
+    return (
+        <div>
+           <a href={project.live}> <h1>{project.title}</h1></a>     
+           <p>{project.description}</p>
+           <h4><a href={project.github}>Github</a><a href={project.live}>Live Demo</a></h4> 
+        </div>
+    )
 }
 
 export default Project;
