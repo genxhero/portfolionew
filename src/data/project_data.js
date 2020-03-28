21e518b2331823ca701d00efc187a98b2b6984d8
genxhero <genxhero@users.noreply.github.com>
2020-03-28 09:19:20 -0700

    lol data

--- src/data/project_data.js
+++ src/data/project_data.js
@@ -14,11 +14,11 @@ const project_data = [
         live: "https://fliquor.herokuapp.com/#/"
     }, 
     {
-        title:
-        image:
-        description:
-        github:
-        live: 
+        title: "Library of Legends",
+        image: "./images/lol.png",
+        description: "A MERN application utilizing GraphQL for creating and storing characters made for the Pathfinder Role Playing Game Second Edition. It displays information to the user with a React front-end and uses GraphQL to make API calls to a Mongo and Express back-end while a character's image is stored on an AWS S3 bucket.",
+        github: "https://github.com/genxhero/libraryoflegends",
+        live: "https://cryptic-plains-92849.herokuapp.com/#/"
     },
     {
         title:
