c63881e5e92691b2bca43369e3050830a72663c5
genxhero <genxhero@users.noreply.github.com>
2020-03-30 23:33:22 -0700

    add way to render

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Projects from './Projects';
 
 const PAGES = [<Projects />]
 
 const MainContent = () => {
+    const [panel, setPanel] = useState(0)
     return (
         <div>
-            
+            {PAGES[panel]}
         </div>
     )
 
