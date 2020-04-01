c711602863d02cfe8eb23b0ea51f44d7f0ab8c2d
genxhero <genxhero@users.noreply.github.com>
2020-03-31 23:08:24 -0700

    add spans to nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const Nav = () => {
-    const [currentPage, setPage] = useState(0)
+    
     return (
         <div>
-
+            <span name="0">Projects</span>
+            <span name="1">Photos</span>
         </div>
     )
 
