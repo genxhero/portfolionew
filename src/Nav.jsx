4047413ce5a00f8a5bc7cf20b8e025ac93c55d71
genxhero <genxhero@users.noreply.github.com>
2020-03-31 23:04:12 -0700

    setpage

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const Nav = () => {
+    const [currentPage, setPage] = useState(0)
+    return (
+        <div>
+
+        </div>
+    )
 
 }
 
