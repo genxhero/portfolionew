e31711918fc20fa1635e6bcef9d855b5c480f3c6
genxhero <genxhero@users.noreply.github.com>
2020-04-02 08:04:21 -0700

    add alt text. snarky alt text

--- src/Project.jsx
+++ src/Project.jsx
@@ -6,7 +6,7 @@ const Project = props => {
         <div>
            <a href={project.live}> <h1>{project.title}</h1></a>     
             <p style={{ "width": "20rem" }}>{project.description}</p>
-           <img style={{"width": "20rem"}} src={project.image} />
+           <img style={{"width": "20rem"}} src={project.image} alt="Shut up, eslint."/>
            <h4><a href={project.github}>Github</a><a href={project.live}>Live Demo</a></h4> 
         </div>
     )
