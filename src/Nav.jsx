7395582219b69017e2d022223385387db329a5f7
genxhero <genxhero@users.noreply.github.com>
2020-04-01 23:13:08 -0700

    add classnames

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,9 +3,9 @@ import React, {useState} from 'react';
 const Nav = (props) => {
     const {navigate} = props
     return (
-        <div>
-            <span onClick={navigate} name="0">Projects</span>
-            <span onClick={navigate} name="1">Photos</span>
+        <div className="nav">
+            <span className="nav-link" onClick={navigate} name="0">Projects</span>
+            <span className="nav-link" onClick={navigate} name="1">Photos</span>
         </div>
     )
 
