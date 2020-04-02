5aaff6a95d9babea4f53b5706c6a8202533d0a97
genxhero <genxhero@users.noreply.github.com>
2020-04-02 08:19:31 -0700

    remove h1 from link, more css

--- src/Project.jsx
+++ src/Project.jsx
@@ -4,7 +4,7 @@ const Project = props => {
     const {project} = props;
     return (
         <div>
-           <a href={project.live}> <h1>{project.title}</h1></a>     
+           <a href={project.live}> {project.title}</a>     
             <p style={{ "width": "20rem" }}>{project.description}</p>
            <img style={{"width": "20rem"}} src={project.image} alt="Shut up, eslint."/>
            <h4><a href={project.github}>Github</a><a href={project.live}>Live Demo</a></h4> 
