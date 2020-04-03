46c2282dd3e94bd52809ac345174922672955745
genxhero <genxhero@users.noreply.github.com>
2020-04-03 14:02:26 -0700

    update links

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,3 +1,9 @@
-const LINKS = ["https://i.imgur.com/urzRarl.jpg"];
+const LINKS = [
+  "https://i.imgur.com/urzRarl.jpg",
+  "https://i.imgur.com/K3lROQB.jpg",
+  "https://i.imgur.com/EwNKDzP.jpg",
+  "https://i.imgur.com/VxgpEYN.jpg",
+  "https://i.imgur.com/t7fhxGO.jpg"
+];
 
 export default LINKS;
