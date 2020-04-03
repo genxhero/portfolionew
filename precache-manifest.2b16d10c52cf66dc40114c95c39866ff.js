7717a70e6d24b74a10def26847a50f737db72d00
genxhero <genxhero@users.noreply.github.com>
2020-04-03 14:10:34 -0700

    Updates

---
+++ precache-manifest.2b16d10c52cf66dc40114c95c39866ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87631d52f7446b9336035a2dd0e5db33",
+    "url": "/portfolionew/index.html"
+  },
+  {
+    "revision": "74eeea18c41b44df990c",
+    "url": "/portfolionew/static/css/main.02014f68.chunk.css"
+  },
+  {
+    "revision": "2b486fe097d983734c5b",
+    "url": "/portfolionew/static/js/2.56889608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolionew/static/js/2.56889608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74eeea18c41b44df990c",
+    "url": "/portfolionew/static/js/main.6483a557.chunk.js"
+  },
+  {
+    "revision": "72f5c8098a2dbd857e9f",
+    "url": "/portfolionew/static/js/runtime-main.d5fa4788.js"
+  }
+]);
