cef640fa0477cc2b59db58d103e8dc677ef4bbc0
genxhero <genxhero@users.noreply.github.com>
2020-04-03 14:33:20 -0700

    add classnames

--- src/Project.jsx
+++ src/Project.jsx
@@ -3,13 +3,24 @@ import React from 'react'
 const Project = props => {
     const {project} = props;
     return (
-        <div className="project">
-           <a href={project.live}> {project.title}</a>     
-            <p style={{ "width": "20rem" }}>{project.description}</p>
-           <img style={{"width": "20rem"}} src={project.image} alt="Shut up, eslint."/>
-           <h4><a href={project.github}>Github</a><a href={project.live}>Live Demo</a></h4> 
+      <div className="project">
+        <a className="project-title" href={project.live}>
+          {" "}
+          {project.title}
+        </a>
+        <p className="project-description">{project.description}</p>
+        <div className="project-thumbnail">
+          <img
+            src={project.image}
+            alt="Shut up, eslint."
+          />
         </div>
-    )
+        <h4>
+          <a href={project.github}>Github</a>
+          <a href={project.live}>Live Demo</a>
+        </h4>
+      </div>
+    );
 }
 
 export default Project;
