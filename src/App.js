a498e7d7d7ed0028d04959bdc3e029bd82603206
genxhero <genxhero@users.noreply.github.com>
2020-04-04 12:20:09 -0700

    fix the daggone image url

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, {useState} from 'react';
 import './App.scss';
 import Nav from './Nav';
 import MainContent from './MainContent';
+import BACKDROPS from './constants/backdrops';
+import {getRandomInt} from './helpers/helpers';
 
 const App = () =>{
   const [page, setPage] = useState(0)
-
+  const backdrop = BACKDROPS[getRandomInt(0, BACKDROPS.length)]
+  console.log(backdrop)
   const navigate = (e) => {
     const index = parseInt(e.target.getAttribute("name"));
     setPage(index)
   }
 
   return ( 
-    <div className="App">
+    <div className="App" style={{"backgroundImage": `url(${backdrop})`}}>
        <Nav navigate={navigate} page={page}/>
        <MainContent page={page}/> 
     </div>
---
+++ src/constants/backdrops.js
@@ -0,0 +1,6 @@
+const BACKDROPS = [
+  "https://ak3.picdn.net/shutterstock/videos/1007299423/thumb/1.jpg",
+  "https://www.plm.automation.siemens.com/media/global/en/agile-sprint-board-kanban_tcm27-55805.jpg",
+];
+
+export default BACKDROPS;
