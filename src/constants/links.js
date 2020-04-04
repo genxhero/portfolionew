ae1633b21a8ae5679f6e17ca98528025abf6a5ba
genxhero <genxhero@users.noreply.github.com>
2020-04-04 15:04:09 -0700

    add more photos

--- src/constants/links.js
+++ src/constants/links.js
@@ -3,7 +3,12 @@ const LINKS = [
   "https://i.imgur.com/K3lROQB.jpg",
   "https://i.imgur.com/EwNKDzP.jpg",
   "https://i.imgur.com/VxgpEYN.jpg",
-  "https://i.imgur.com/t7fhxGO.jpg"
+  "https://i.imgur.com/t7fhxGO.jpg",
+  "https://i.imgur.com/mzUQ0Mg.jpg",
+  "https://i.imgur.com/Bw3LBwr.jpg",
+  "https://i.imgur.com/4txKPlm.jpg",
+  "https://i.imgur.com/puZxoD2.jpg",
+  "https://i.imgur.com/TclNZbl.jpg",
 ];
 
 export default LINKS;
