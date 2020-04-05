73cc4eeaafccd518f7f7114facc786b32a588a45
genxhero <genxhero@users.noreply.github.com>
2020-04-05 09:26:04 -0700

    add transition to photos

--- src/Photos.jsx
+++ src/Photos.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Photo from './Photo';
 import LINKS from './constants/links';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 
 const Photos = props => {
+ 
     return (
-        <div>
+    <TransitionGroup>
+        <CSSTransition timeout={{ enter: 300, exit: 300 }}  classNames={'photos'}>
+        <div className="photos">
             <div className="photos-intro" >
                 <p>I am also a photographer - always looking for new event shoots, commissions, and models to expand my portfolio.  Below is a sample of my work</p>
             </div>
@@ -13,6 +18,8 @@ const Photos = props => {
                 {LINKS.map( url => <Photo url={url} key={url}/>)}
             </div>
         </div>
+           </CSSTransition>
+      </TransitionGroup>
     )
 }
 
