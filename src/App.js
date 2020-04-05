c5314132a7b4a66cf01d5f1ce7420c1ade78c9c8
genxhero <genxhero@users.noreply.github.com>
2020-04-05 09:24:56 -0700

    animation

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ import Background from './Background';
 import Intro from './Intro';
 
 const App = () =>{
-  const [page, setPage] = useState(0)
-
-  // useEffect(() => {
-  //   const interval = setInterval((BACKDRROPS) => {
-  //     let newBackdrop = backdrop === (BACKDROPS.length -1 ) ? 0 : (backdrop + 1);
-  //     cycleBackdrop(newBackdrop);
-  //     $("App").fadeIn();
-  //   }, 2000);
-  //   return () => clearInterval(interval);
-  // }, [backdrop])
-
+  const [page, setPage] = useState(0);
 
   const navigate = (e) => {
     const index = parseInt(e.target.getAttribute("name"));
