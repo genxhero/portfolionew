c499a7fa82a32edca3fba0e51560648415fac696
genxhero <genxhero@users.noreply.github.com>
2020-04-06 14:14:22 -0700

    start up an AB test toggle

---
+++ src/widgets/AbTestToggle.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * Expected Props 
+ *
+ * toggle: Function which mutates the state of the parent component, otherwise a noop
+ * 
+ * TODO: Get propTypes up in here.
+ * 
+ * @param {*} props 
+ */
+const AbTestToggle = props => {
+    return (
+        <div className="ab-test-toggle">
+
+        </div>
+    )
+}
