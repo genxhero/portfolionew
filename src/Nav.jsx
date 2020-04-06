7d9ac115a9f0b7a3c0372efc305c8be003c00551
genxhero <genxhero@users.noreply.github.com>
2020-04-06 14:31:04 -0700

    conditional render of third link

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Nav = (props) => {
-    const {navigate, page} = props
+    const {navigate, page, testGroup} = props
     console.log(props)
     return (
         <div className="nav">
             <span className={`nav-link ${page === 0 ? 'active': ''}`} onClick={navigate} name="0">Projects</span>
             <span className={`nav-link ${page === 1 ? 'active' : ''}`} onClick={navigate} name="1">Photos</span>
+            {testGroup === "b" && <span className={`nav-link ${page === 1 ? 'active' : ''}`} onClick={navigate} name="2">About Me</span>}
         </div>
     )
 
