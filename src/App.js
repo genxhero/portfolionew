ba7feae69a83cf9a02a6caefa8e6c09ff53458c0
genxhero <genxhero@users.noreply.github.com>
2020-04-06 14:41:29 -0700

    import and export abtest toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './Nav';
 import MainContent from './MainContent';
 import Background from './Background';
 import Intro from './Intro';
+import AbTestToggle from './widgets/AbTestToggle';
 
 const App = () =>{
   const [page, setPage] = useState(0);
@@ -18,6 +19,7 @@ const App = () =>{
        <Intro testGroup={testGroup}/>
        <Nav navigate={navigate} page={page} testGroup={testGroup}/>
        <MainContent page={page} testGroup={testGroup}/> 
+       <AbTestToggle toggle={setTestGroup}/>
        <Background />
     </div>
   );
--- src/widgets/AbTestToggle.jsx
+++ src/widgets/AbTestToggle.jsx
@@ -16,3 +16,5 @@ const AbTestToggle = props => {
         </div>
     )
 }
+
+export default AbTestToggle;
