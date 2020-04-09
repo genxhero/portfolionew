253c5bf03ab7ce5c252d18cba1a2d696313660d3
genxhero <genxhero@users.noreply.github.com>
2020-04-09 13:06:11 -0700

    classnames to other elements

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -9,15 +9,23 @@ const AboutMe = props => {
     return (
       <div className={`about-me ${props.top ? "top" : ""}`}>
         <h1>About Me</h1>
-        <p className="about-me-section">
-          I'm a career-driven and tenacious software develope.  Born and bred in Southern California, I grew up around the time the
-          internet was really starting to take off. As a result, I've always
-          immersed myself in technology. What I like about web development is
-          the gestalt, the omnibus: every little thing about it fills me with
-          exhilaration! The way I an apply my problem solving skills (and love
-          for puzzles), the way I can exercise my creativity on a canvas of
-          code, I could go on forever!
-        </p>
+        <div className="about-me-section">
+          <h3 className="about-me-subtitle">History</h3>
+          <p className="about-me-text">
+             Born and bred in Southern California, I grew up around the time the internet was
+            really starting to take off. As a result, I've always immersed
+            myself in technology. What I like about web development is the
+            gestalt, the omnibus: every little thing about it fills me with
+            exhilaration! The way I an apply my problem solving skills (and love
+            for puzzles), the way I can exercise my creativity on a canvas of
+            code, I could go on forever!
+          </p>
+        </div>
+
+        <div className="about-me-section reverse">
+          <h3 className="about-me-subtitle">Skills</h3>
+          <ul className="about-me-list"></ul>
+        </div>
       </div>
     );
 }
