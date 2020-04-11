afb9e02f640cf664e0af3ca0446497ab3f91a57c
genxhero <genxhero@users.noreply.github.com>
2020-04-11 09:12:36 -0700

    create styles from props

--- src/widgets/Tooltip.jsx
+++ src/widgets/Tooltip.jsx
@@ -1,9 +1,11 @@
 import React from 'react'; 
+import PropTypes from 'prop-types';
 
 const Tooltip = props => {
     const {background, color, message, direction} = props;
+    const style = {"color":color, "background": background}
     return (
-        <div className="tooltip">
+        <div className="tooltip" style={style}>
         </div>
     )
 }
