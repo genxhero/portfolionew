6f054dd261b675be207aac587359d6f908ac6556
genxhero <genxhero@users.noreply.github.com>
2020-04-11 09:09:35 -0700

    tooltip props set up

---
+++ src/widgets/Tooltip.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Tooltip = props => {
+    const {background, color, message, direction} = props;
+    return (
+        <div className="tooltip">
+        </div>
+    )
+}
+
+export default Tooltip;
