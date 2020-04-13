bc33b12c27c9f08d03f33bac327bcd1cf7bf1f25
genxhero <genxhero@users.noreply.github.com>
2020-04-12 23:27:52 -0700

    prop types for photo

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Expected Props
@@ -13,6 +14,10 @@ const Photo = props => {
             <img alt="this is an alt. under protest" src={props.url}/>
         </div>
     )
+}
+
+Photo.propTypes = {
+    url: PropTypes.string
 }
 
 export default Photo;
