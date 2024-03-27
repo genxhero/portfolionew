4211abb4bb40edada222afaa1a3b0283c37f1d35
aaronbear@gmail.com <=>
2024-03-27 10:40:39 -0700

    refactor image background to load files locally

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,22 +1,32 @@
 import React, {useState, useEffect} from 'react';
-import BACKDROPS from "./constants/backdrops";
+//import BACKDROPS from "./constants/backdrops";
 import $ from 'jquery';
+import BG1 from "./images/BG01.jpg";
+import BG2 from "./images/BG02.jpg";
+import BG3 from "./images/BG03.jpg";
+import BG4 from "./images/BG04.jpg";
+import BG5 from "./images/BG05.jpg";
+import BG6 from "./images/BG06.jpg";
+import BG7 from "./images/BG07.jpg";
+
 
 /**
  * Rotating Background
  * TODO: Refactor to use CSS Transition.
  * @param {*} props 
  */
+
+const BACKDROPS = [BG1, BG2, BG3, BG4, BG5, BG6, BG7]
+
 const Background = (props) => {
     const [index, cycleBackdrop] = useState(0);
 
     //
     const images = preloadImages(BACKDROPS);
 
-    //Item 1: Change BACKDROPS to be an array of images stored in the main folder
 
     useEffect(() => {
-        const interval = setInterval((BACKDRROPS) => {
+        const interval = setInterval( () => {
         let newIndex = index === (BACKDROPS.length -1 ) ? 0 : (index + 1);
         cycleBackdrop(newIndex);
         $("background-image").fadeIn();
--- src/constants/backdrops.js
+++ src/constants/backdrops.js
@@ -1,12 +1,22 @@
+// const BACKDROPS = [
+//   "https://ak3.picdn.net/shutterstock/videos/1007299423/thumb/1.jpg",
+//   "https://www.plm.automation.siemens.com/media/global/en/agile-sprint-board-kanban_tcm27-55805.jpg",
+//   "https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+//   "https://wallpapercave.com/wp/wp1828913.jpg",
+//   "https://www.itl.cat/pngfile/big/82-822314_uhd-printed-circuit-board.jpg",
+//   "https://c4.wallpaperflare.com/wallpaper/193/203/796/futuristic-tech-geometry-cyan-wallpaper-preview.jpg",
+// ];
+
+// export default BACKDROPS;
+
 const BACKDROPS = [
-  "https://ak3.picdn.net/shutterstock/videos/1007299423/thumb/1.jpg",
-  "https://www.plm.automation.siemens.com/media/global/en/agile-sprint-board-kanban_tcm27-55805.jpg",
-  "https://wallpaperplay.com/walls/full/5/a/a/77451.jpg",
-  "https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-  "https://wallpapercave.com/wp/wp1828913.jpg",
-  "https://www.itl.cat/pngfile/big/82-822314_uhd-printed-circuit-board.jpg",
-  "https://cdn.hipwallpaper.com/i/77/0/5WKDzl.jpg",
-  "https://c4.wallpaperflare.com/wallpaper/193/203/796/futuristic-tech-geometry-cyan-wallpaper-preview.jpg",
-];
+        "./images/BG01.jpg",
+        "./images/BG02.jpg",
+        "./images/BG03.jpg",
+        "./images/BG04.jpg",
+        "./images/BG06.jpg",
+        "./images/BG06.jpg",
+        "./images/BG07.jpg"
+      ]
 
 export default BACKDROPS;
